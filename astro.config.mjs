021ff1e40066fe81694acb3cf4e91e207b3e6420
kqmaverick <kqmaverick@gmail.com>
2024-04-22 11:20:05 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,14 +12,7 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					autogenerate: { directory: 'guides' },
 				},
 			],
 		}),
